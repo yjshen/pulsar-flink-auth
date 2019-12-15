186d985b155dd5e0de5801528ff835f8cee05810
Yijie Shen <henry.yijieshen@gmail.com>
2019-12-15 15:53:33 +0800

    pulsar sink as well

--- src/test/scala/com/example/AuthTest.scala
+++ src/test/scala/com/example/AuthTest.scala
@@ -2,34 +2,46 @@ package com.example
 
 import java.nio.charset.StandardCharsets
 
-import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.serialization.{SerializationSchema, SimpleStringSchema}
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.functions.source.SourceFunction
-import org.apache.flink.streaming.connectors.pulsar.PulsarSourceBuilder
+import org.apache.flink.streaming.connectors.pulsar.partitioner.PulsarKeyExtractor
+import org.apache.flink.streaming.connectors.pulsar.{FlinkPulsarProducer, PulsarSourceBuilder}
 import org.apache.pulsar.client.api.{MessageId, PulsarClient}
+import org.apache.pulsar.client.impl.conf.{ClientConfigurationData, ProducerConfigurationData}
 import org.apache.pulsar.common.naming.TopicName
 
 class AuthTest extends FlinkTest {
 
-  val SUBSCRIPTION_NAME = "test1"
+  val SUBSCRIPTION_NAME = "test1222"
   val AUTH_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5aWppZSJ9.I-v7SN9cUmQOqKsqYowUvTjADutJ1uiMb1jkK0c5ihs"
   val SERVICE_URL = "pulsar://localhost:6650"
-  val INPUT_TOPIC = TopicName.get("tp1").toString
+  val INPUT_TOPIC = TopicName.get("tp2").toString
+  val OUTPUT_TOPIC = TopicName.get("tp-out-2").toString
 
   test("auth") {
 
     val see = StreamExecutionEnvironment.getExecutionEnvironment
     see.getConfig.disableSysoutLogging()
+    see.setParallelism(1)
 
     val messages = Array("A", "B", "C")
 
     sendMessages(INPUT_TOPIC, messages, SERVICE_URL)
 
+    val confData = new ClientConfigurationData()
+    confData.setAuthPluginClassName("org.apache.pulsar.client.impl.auth.AuthenticationToken")
+    confData.setAuthParams("token:" + AUTH_TOKEN)
+
+    val prodData = new ProducerConfigurationData()
+    prodData.setTopicName(OUTPUT_TOPIC)
+
     val src: SourceFunction[String] =
       PulsarSourceBuilder
         .builder(new SimpleStringSchema())
-        .authentication("org.apache.pulsar.client.impl.auth.AuthenticationToken", "token:" + AUTH_TOKEN)
+        .pulsarAllClientConf(confData)
+        //.authentication("org.apache.pulsar.client.impl.auth.AuthenticationToken", "token:" + AUTH_TOKEN)
         .serviceUrl(SERVICE_URL)
         .topic(INPUT_TOPIC)
         .subscriptionName(SUBSCRIPTION_NAME)
@@ -37,8 +49,19 @@ class AuthTest extends FlinkTest {
 
     implicit val typeInfo = BasicTypeInfo.STRING_TYPE_INFO
 
-    val dataStream = see.addSource(src)
-    dataStream.print()
+    val ds = see.addSource(src)
+
+    ds.addSink(
+      new FlinkPulsarProducer[String](
+        confData,
+        prodData,
+        new SerializationSchema[String] {
+          override def serialize(t: String): Array[Byte] = t.getBytes(StandardCharsets.UTF_8)
+        },
+        new PulsarKeyExtractor[String] {
+          override def getKey(in: String): String = "test"
+        }
+      ))
 
     see.execute("flink pulsar api")
 
